fa0d5167cbbffc295d9f8125ba86aef032055700
janyanisanjay <43444350+janyanisanjay@users.noreply.github.com>
2020-03-31 05:03:53 +0530

    Update forgot_mail.php

--- classes/higher_authority/phpmailer/forgot_mail.php
+++ classes/higher_authority/phpmailer/forgot_mail.php
@@ -23,7 +23,7 @@ try {
     $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
     $mail->Username = 'sanjayjanyani43@gmail.com';                 // SMTP username
-    $mail->Password = '9730319048';                           // SMTP password
+    $mail->Password = '*****';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 
